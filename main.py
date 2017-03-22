3f2152579d3a45519b5333f8babc69080a5c48c5
7275639 <jake.davidson@mines.sdsmt.edu>
2017-03-22 16:36:48 -0600

    removed unused import

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from DataFetch import *
 from Simulations import *
-import collections
-
 
 def main():
     # currently prints true if random group of 23 dates
     # from data.txt contains duplicates
-    
+
     birthdays = getDatesFromFile('data.txt')
     print(simulateBirthdayProblem(birthdays, 23))
 
