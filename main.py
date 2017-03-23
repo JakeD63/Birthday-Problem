b8396736b9d25dd6db7e862c71d7985e580c448d
Jake <jake.davidson@mines.sdsmt.edu>
2017-03-22 21:44:32 -0600

    main now compares sample data with rand data and outputs stats about it

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ def main():
     # currently prints true if random group of 23 dates
     # from data.txt contains duplicates
     sampleSize = 23
-    birthdays = generateRandomBirthdays(sampleSize)
-    print(simulateBirthdayProblem(birthdays, sampleSize))
+    timesToRun = 1000
+    populationSample = getDatesFromFile('data.txt')
+    populationResult, randomResult = compareSampleWithRandomData(populationSample, timesToRun, sampleSize)
+    popPercent = float(populationResult) / timesToRun * 100
+    randPercent = float(randomResult) / timesToRun * 100
 
+    print("Simulations Run: " + str(timesToRun) + " Sample Size: " + str(sampleSize))
+    print("Population Sample Stats: " + str(popPercent) + "% Success")
+    print("Randomly Generated Sample Stats: " + str(randPercent) + "% Success")
 
 if __name__ == '__main__':
     main()
