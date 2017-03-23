b0b77f541b881c4faaa1c118e19550f7c2c8c04c
Jake <jake.davidson@mines.sdsmt.edu>
2017-03-22 21:15:28 -0600

    changed import and tested new function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from DataFetch import getDatesFromFile
-from Simulations import simulateBirthdayProblem
+from Simulations import *
 
 def main():
     # currently prints true if random group of 23 dates
     # from data.txt contains duplicates
-
-    birthdays = getDatesFromFile('data.txt')
-    print(simulateBirthdayProblem(birthdays, 23))
+    sampleSize = 23
+    birthdays = generateRandomBirthdays(sampleSize)
+    print(simulateBirthdayProblem(birthdays, sampleSize))
 
 
 if __name__ == '__main__':
