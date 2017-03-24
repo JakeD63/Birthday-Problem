d33e11ef71ba011695bb443c8692da45627c4cd8
Jake Davidson <jake.davidson@mines.sdsmt.edu>
2017-03-24 15:02:13 -0600

    changed stuff

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ def testRun():
     timesToRun = 1000
     groupSize = 23
     populationSample = getDatesFromFile('data.txt')
+
     randomSample = generateRandomBirthdays(len(populationSample))
     weightedSample = generateWeightedBirthdays(populationSample, len(populationSample))
-    results = compareSamples([populationSample, weightedSample, randomSample], timesToRun, groupSize)
 
-    outputComparison(results, ["CSC Population Sample", "Weighted Random Sample", "Pure Random Sample"], timesToRun, groupSize)
+    results = compareSamples([populationSample, weightedSample, randomSample], timesToRun, groupSize)
+    sampleNames = ["CSC Population Sample", "Weighted Random Sample", "Pure Random Sample"]
+    outputComparison(results, sampleNames, timesToRun, groupSize)
 
 
 if __name__ == '__main__':
