3330d3b83e4ea5a7c3e4fd24c7a6cf8d53b892ba
Jake <jake.davidson@mines.sdsmt.edu>
2017-04-16 21:37:46 -0600

    added mean gathering. (currently not called in main)

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 from DataFetch import getDatesFromFile
 from Simulations import *
-
+from sys import stdout
 def main():
     testRun()
 
 def testRun():
     timesToRun = 1000
     groupSize = 23
+    mean = 0
     populationSample = getDatesFromFile('data.txt')
 
     randomSample = generateRandomBirthdays(len(populationSample))
     weightedSample = generateWeightedBirthdays(populationSample, len(populationSample))
 
     results = compareSamples([populationSample, weightedSample, randomSample], timesToRun, groupSize)
+
     sampleNames = ["CSC Population Sample", "Weighted Random Sample", "Pure Random Sample"]
     outputComparison(results, sampleNames, timesToRun, groupSize)
 
+def getMean():
+    timesToRun = 1000
+    groupSize = 23
+    mean = 0
+    populationSample = getDatesFromFile('data.txt')
+
+    randomSample = generateRandomBirthdays(len(populationSample))
+    weightedSample = generateWeightedBirthdays(populationSample, len(populationSample))
+
+    #run simulatino 100 times, get mean difference in success rates
+    for i in range(0, 100):
+        results = compareSamples([weightedSample, randomSample], timesToRun, groupSize)
+        mean += results[0] - results[1]
+        stdout.write(str((results[0] - results[1])))
+        stdout.write(', ')
+    stdout.write('\n')
+    mean = float(mean) / 100
+    print('Mean: ' , mean)
 
 if __name__ == '__main__':
     main()
